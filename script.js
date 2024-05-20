839613526d86c402bf007fcfaede832d1d8c441d
Bilqis <omoyeniayeni@gmail.com>
2024-05-19 22:39:08 -0700

    2024 v4

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // This script allows you to easily update the daily affirmation and photo.
 document.addEventListener("DOMContentLoaded", function() {
-    const dailyAffirmation = `And when fear or doubt intrudes, I pray that God grant you  courage and confidence. 
-    As you continue to prepare for your exam, may God guide. You are capable ❤️`;
+    const dailyAffirmation = `"I embrace this day with confidence and trust in God's guidance. 
+    I am calm, focused, and ready for this exam. 
+    My preparation has been thorough, and I trust in my ability to perform well."
+        You've got this babe! 😘`;
     const dailyPhotoSrc = "path-to-todays-photo.jpg"; // Update this path daily
 
     document.getElementById('daily-affirmation').textContent = dailyAffirmation;
